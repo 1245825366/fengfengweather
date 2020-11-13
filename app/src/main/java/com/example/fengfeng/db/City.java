2cafb4e94db8b56fb1d867254fd53ba5c82cfcf7
djl <1245825366@qq.com>
2020-11-13 16:38:52 +0800

    First commit.

---
+++ app/src/main/java/com/example/fengfeng/db/City.java
@@ -0,0 +1,31 @@
+package com.example.fengfeng.db;
+
+public class City {
+    private int id;
+    private String provinceNNNName;
+    private int provienceCod;
+
+    public int getProvienceCod() {
+        return provienceCod;
+    }
+
+    public void setProvienceCod(int provienceCod) {
+        this.provienceCod = provienceCod;
+    }
+
+    public String getProvinceNNNName() {
+        return provinceNNNName;
+    }
+
+    public void setProvinceNNNName(String provinceNNNName) {
+        this.provinceNNNName = provinceNNNName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
